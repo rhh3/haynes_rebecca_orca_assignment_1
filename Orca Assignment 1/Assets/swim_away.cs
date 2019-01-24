15a819d3400d2c7905b77007e9a40cae553a5a79
Rebecca Haynes <beccah.haynes@gmail.com>
2019-01-23 19:34:54 -0800

    here are all the files

---
+++ Orca Assignment 1/Assets/swim_away.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swim_away : MonoBehaviour
+{
+    private void OnTriggerStay(Collider collision)
+    {
+        print("working");
+        transform.position -= Vector3.right * Time.deltaTime;
+    }
+}
